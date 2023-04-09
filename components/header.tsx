0707dead0e490cd1cd84868de85cb68138dd3104
FelipeNsol <felipe@nsolucoes.digital>
2023-04-09 12:50:16 -0300

    Added basic header and styles

---
+++ components/header.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Image from 'next/image'
+import { Source_Sans_Pro } from 'next/font/google'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import {FiSearch} from 'react-icons/fi'
+
+const source_sans_pro = Source_Sans_Pro({ subsets: ['latin'], weight: '600' })
+
+const Header = () => {
+    const router = useRouter()
+    const [linkTo, setLinkTo] = useState('')
+    function formHandler(e: React.FormEvent<HTMLFormElement>) {
+        e.preventDefault()
+        if (linkTo !== '') {
+           router.push(`/city/${linkTo}`) 
+        }
+    }
+    return (
+        <header>
+            <span>
+                <Image src='/img/sun rain.png'
+                    alt="Picture of the author"
+                    width={60}
+                    height={60} />
+                <h1 className={source_sans_pro.className}>WeatherTracker</h1>
+            </span>
+            <form onSubmit={formHandler}>
+                <input type='text' onChange={e => setLinkTo(e.target.value)} placeholder='Search for a city'/>
+                <button type='submit'>{<FiSearch size={22}/>}</button>
+            </form>
+        </header>
+    )
+}
+
+export default Header
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import '@/styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Header from '@/components/header'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <Component {...pageProps} />
+    <>
+      <Header />
+      <Component {...pageProps} />
+    </>
   )
 }
