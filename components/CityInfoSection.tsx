246f09c69d175b862c0cfd836646c978c74bb9de
FelBenini <felbenini@gmail.com>
2023-04-12 20:43:46 -0300

    Now the api called every minute to keep UI updated

--- components/CityInfoSection.tsx
+++ components/CityInfoSection.tsx
@@ -16,29 +16,34 @@ const CityInfoSection = ({ cityName }: { cityName: string | undefined | string[]
     const { city } = useContext(CityContext)
 
     const fetchData = async (cityName: string | undefined | string[]) => {
-        setLoading(true)
         if (cityName !== '' && cityName !== undefined) {
             const { data } = await axios.get(`/api/city/${cityName}`)
             setInfo(data)
             setLoading(false)
-            console.log(cityName)
+
+            const fetchAgain = setTimeout(() => {
+                fetchData(cityName)
+            }, 60000);
+            return () => clearTimeout(fetchAgain);
         } else {
         }
     }
+
     useEffect(() => {
         fetchData(cityName)
     }, [city, cityName])
+
     if (info !== null && !loading) {
         return (
             <>
-                <section id='citySection' style={{backgroundImage: `linear-gradient(to top, #151617, #22242590, #22242580) ,url(${info.bgImg})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+                <section id='citySection' style={{ backgroundImage: `linear-gradient(to top, #151617, #22242590, #22242580) ,url(${info.bgImg})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
                     <h1 className={source_sans_pro.className}>{info.data.location.name}
-                    <h4>{info.data.location.country}</h4>
+                        <h4>{info.data.location.country}</h4>
                     </h1>
                     <h3 className={notoSans.className}>Real time Weather</h3>
                     <span className={`currentWeather`}>
-                        <h2 className={notoSans.className}>{info.data.current.temp_c}º C<img src={info.data.current.condition.icon}/></h2>
-                        <h2 className={notoSans.className}>{info.data.current.temp_f}º F<img src={info.data.current.condition.icon}/></h2>
+                        <h2 className={notoSans.className}>{info.data.current.temp_c}º C<img src={info.data.current.condition.icon} /></h2>
+                        <h2 className={notoSans.className}>{info.data.current.temp_f}º F<img src={info.data.current.condition.icon} /></h2>
                     </span>
                 </section>
             </>
