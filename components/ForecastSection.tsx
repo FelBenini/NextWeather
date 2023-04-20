152dcfcae8b458d68a060be8defc0a292d12deb7
FelBenini <felbenini@gmail.com>
2023-04-20 12:03:13 -0300

    Change img to Image on forecast section

--- components/ForecastSection.tsx
+++ components/ForecastSection.tsx
@@ -4,6 +4,7 @@ import { CityContext } from './userCity'
 import { Skeleton } from '@mui/material'
 import { Noto_Sans } from 'next/font/google'
 import { Source_Sans_Pro } from 'next/font/google'
+import Image from 'next/image'
 
 const notoSans = Noto_Sans({ subsets: ['latin'], weight: '400' })
 const source_sans_pro = Source_Sans_Pro({ subsets: ['latin'], weight: '600' })
@@ -36,7 +37,7 @@ const ForecastSection = ({ cityName }: { cityName: string | undefined | string[]
         const mappedInfo = info.data.map((element: any, index: any) => {
             return (
                 <div className='dayCard dayCardReady' key={index}>
-                    <h4>{epochToDate(element.date_epoch)} <img src={element.day.condition.icon}/><h5 className={notoSans.className}> - {element.day.condition.text}</h5></h4>
+                    <h4>{epochToDate(element.date_epoch)} <Image src={'https:'+element.day.condition.icon} width={32} height={32} alt={element.day.condition.text}/><h5 className={notoSans.className}> - {element.day.condition.text}</h5></h4>
                     <div>
                     <span>
                         <h5>Min temp</h5>
@@ -61,7 +62,7 @@ const ForecastSection = ({ cityName }: { cityName: string | undefined | string[]
 
         return (
             <section className={notoSans.className} id='forecastSection'>
-                <h1 className={source_sans_pro.className}>Forecast <h5 className={notoSans.className}>Next seven days</h5></h1>
+                <h1 className={source_sans_pro.className}>Forecast <h5 className={notoSans.className}>Next days</h5></h1>
                 <div className='wrapperInline'>
                     {mappedInfo}
                 </div>
@@ -70,7 +71,7 @@ const ForecastSection = ({ cityName }: { cityName: string | undefined | string[]
     } else {
         return (
             <section id='forecastSection'>
-                <h1 className={source_sans_pro.className}>Forecast <h5 className={notoSans.className}>Next seven days</h5></h1>
+                <h1 className={source_sans_pro.className}>Forecast <h5 className={notoSans.className}>Next days</h5></h1>
                 <div className='wrapperInline'>
                     <Skeleton className='dayCard' variant="rounded" width={`90%`} height={64} />
                     <Skeleton className='dayCard' variant="rounded" width={`90%`} height={64} />
